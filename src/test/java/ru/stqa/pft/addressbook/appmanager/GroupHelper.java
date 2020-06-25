60990e46467271cb93b5ffd4a04f46f506055c66
OCHIR08 <41266047+OCHIR08@users.noreply.github.com>
2020-06-25 18:18:12 +0300

    Рефакторинг
    Вынес предуловия для тестов: GroupModificationTests/GroupDeletionTests

--- src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -53,6 +53,14 @@ public class GroupHelper extends HelperBase {
     returnToGroupPage();
   }
 
+  public void modifyGoup(int index, GroupData group) {
+    selectGroup(index);
+    initGroupModification();
+    fillGroupForm(group);
+    submitGroupModification();
+    returnToGroupPage();
+  }
+
   public boolean isThereAGroup() {
     return isElementPresent(By.name("selected[]"));
   }
