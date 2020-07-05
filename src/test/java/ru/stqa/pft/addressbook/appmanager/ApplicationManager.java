ded5c7e824c33dec0d242d8a2c6b3b418cba1b7a
OCHIR08 <41266047+OCHIR08@users.noreply.github.com>
2020-07-06 00:52:27 +0300

    Описание конфигурации тестового стенда

--- src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,10 +6,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  protected WebDriver driver;
+  private final Properties properties;
+  protected WebDriver driver = new FirefoxDriver();
 
   private ContactHelper contactHelper;
   private SessionHelper sessionHelper;
@@ -17,10 +23,14 @@ public class ApplicationManager {
   private  GroupHelper groupHelper;
   private String browser;
 
-  public ApplicationManager(String browser) {
+  public ApplicationManager(String browser){
     this.browser = browser;
+    properties = new Properties();
   }
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
     if  (browser.equals(BrowserType.FIREFOX)) {
       driver = new FirefoxDriver();
     } else if (browser.equals(BrowserType.CHROME)) {
@@ -28,13 +38,13 @@ public class ApplicationManager {
     } else if (browser.equals(BrowserType.IE)) {
       driver = new InternetExplorerDriver();
     }
-    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-    driver.get("http://localhost/addressbook/");
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    driver.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(driver);
     navigationHelper = new NavigationHelper(driver);
     sessionHelper = new SessionHelper(driver);
     contactHelper = new ContactHelper(driver);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
   }
 
   public void stop() {
