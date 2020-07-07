261d7628959b4cfb8d44e050d0c9f33bb338d577
OCHIR08 <41266047+OCHIR08@users.noreply.github.com>
2020-07-07 19:04:15 +0300

    Сравнение данных, загруженных из БД

--- src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -22,6 +22,7 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private  GroupHelper groupHelper;
   private String browser;
+  private DbHelper dbHelper;
 
   public ApplicationManager(String browser){
     this.browser = browser;
@@ -30,6 +31,8 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    dbHelper = new DbHelper();
 
     if  (browser.equals(BrowserType.FIREFOX)) {
       driver = new FirefoxDriver();
@@ -61,5 +64,9 @@ public class ApplicationManager {
 
   public ContactHelper getContactHelper() {
     return contactHelper;
+  }
+
+  public DbHelper db() {
+    return dbHelper;
   }
 }
--- src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -15,6 +15,7 @@ import java.util.Objects;
 @Entity
 @Table(name = "group_list" )
 public class GroupData {
+
   @XStreamOmitField
   @Id
   @Column(name = "group_id")
@@ -77,18 +78,20 @@ public class GroupData {
             ", name='" + name + '\'' +
             '}';
   }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
     return id == groupData.id &&
-            Objects.equals(name, groupData.name);
+            Objects.equals(name, groupData.name) &&
+            Objects.equals(header, groupData.header) &&
+            Objects.equals(footer, groupData.footer);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, name);
+    return Objects.hash(id, name, header, footer);
   }
+
 }
